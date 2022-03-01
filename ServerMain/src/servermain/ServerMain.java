0421e01a26ffc99a0e574eca814fd5109ea2a192
David186107 <98074157+David186107@users.noreply.github.com>
2022-03-01 04:38:43 -0700

    Revert "Revert "inicio del programa""
    This reverts commit 6222021fb289967800aef88eaecb431a7a189481.

---
+++ ServerMain/src/servermain/ServerMain.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package servermain;
+
+/**
+ *
+ * @author david
+ */
+public class ServerMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo!");
+    }
+    
+}
