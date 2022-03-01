6ca91976b6419aae62f54af9b157e83096f5680f
David186107 <98074157+David186107@users.noreply.github.com>
2022-03-01 04:55:33 -0700

    Especificaciones 1
    Se han agregado las primeras especificaciones de lo que trataran las clases, de mismo modo se separo en 3 capas para la división del programa.

---
+++ ServerMain/src/DataBase/GroupArchive.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DataBase;
+
+/**
+ *
+ * @author david
+ */
+public class GroupArchive {
+    //Este metodo se encargara de hacer conexiones entre el sistema y los archivos
+    //de una carpeta correspondiente que se sera designada como base de datos
+}
---
+++ ServerMain/src/Servidor/Conexion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Servidor;
+
+/**
+ *
+ * @author david
+ */
+public class Conexion {
+    //esta clase se encargara de hacer una conexion entre server y cliente
+    //una vez el cliente haga una conexion con el servidor, se pasara a tomar
+    //los datos de el cliente para enviar los datos a el una vez todo este listo
+}
